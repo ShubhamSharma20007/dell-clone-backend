803081fd6c020177e9c06c3c7bfac90ce9b2009a
shubham <shubhamsharma20007@gmail.com>
2024-08-01 23:54:59 +0530

    bcrypt js implement

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const RegisterModel = require('../schema/register.schema');
 const jwt =  require('jsonwebtoken')
 const userRegister = async (req, res) => {
